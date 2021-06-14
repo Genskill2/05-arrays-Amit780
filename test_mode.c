05059bbbde837ba6f93cb211875b8cd854e71d5d
Amit780 <85019843+Amit780@users.noreply.github.com>
2021-06-14 12:56:53 +0530

    Update test_mode.c

--- test_mode.c
+++ test_mode.c
@@ -12,28 +12,3 @@ int main(void) {
   assert (mode(y, 1) == 5);
   printf("Mode: passed\n");
   }
-int mode(int arr[], int len){
-  int ar2[len];
-  int n,p, max;
-  int cnt = 0;
-  for(int i = 0; i< len; i++){
-  n = arr[i];
-  for(int j=0; j<len; j++){
-  if( arr[j] == n)
-  cnt = cnt +1;
-  }
-  ar2[i] = cnt;
-  cnt = 0;
-  }
-  max = ar2[0];
-  for(int h = 0; h< len; h++){
-  if(ar2[h]> max){
-  max = ar2[h];
-  }
-  }
-  for(p =0; p<len; p++){
-  if(max == ar2[p])
-  break;
-  }
-  return arr[p];
-  }
