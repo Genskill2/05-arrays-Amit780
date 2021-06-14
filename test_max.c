6a3b43f70b06e2ee6fb8aeab68f4169f4e140d9e
Amit780 <85019843+Amit780@users.noreply.github.com>
2021-06-14 12:55:20 +0530

    Update test_max.c

--- test_max.c
+++ test_max.c
@@ -11,11 +11,4 @@ int main(void) {
   int y[] = {5};
   assert (max(y, 1) == 5);
   printf("Max: passed\n");
-  }int max(int arr[], int len){
-  int maximum = arr[0];
-  for(int i= 0; i< len; i++){
-    if(arr[i]> maximum)
-      maximum = arr[i];
   }
-  return maximum;
-       }
